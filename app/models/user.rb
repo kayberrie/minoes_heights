aee9f312dac4e92dc50d3cad526fc9b6b98086fb
Khanya Mbini <khanya@go2africa.com>
2017-05-29 21:11:47 +0200

    first stuff

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  surname     :string
+#  room_number :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class User < ApplicationRecord
+end
