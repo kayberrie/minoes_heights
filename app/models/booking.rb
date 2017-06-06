120a1c700bd08332a00911eaeb9205c6583792d9
Khanya Mbini <khanya@go2africa.com>
2017-06-06 22:57:20 +0200

    add service item working

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
-class Booking <ApplicationController
-  belongs to :user
-  belongs to :service_item
+class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :service_item
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,12 +2,13 @@
 #
 # Table name: services
 #
-#  id          :integer          not null, primary key
-#  name        :string
-#  user_id     :string
-#  description :text
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id               :integer          not null, primary key
+#  name             :string
+#  user_id          :string
+#  description      :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  service_category :integer
 #
 
 class Service < ApplicationRecord
